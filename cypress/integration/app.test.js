d659f2d0d75d97c80aa797ed0edd5c8bd0815211
Allan Hermann <allan.hermann@gmail.com>
2022-05-04 17:56:06 -0400

    feat: created login page test

---
+++ cypress/integration/app.test.js
@@ -0,0 +1,15 @@
+describe("UI test", () => {
+  it("should test the login page", () => {
+    //This first tests analyse if the Button continues disabled if the Textbox OR the Checkbox are not filled
+    cy.visit("http://localhost:3000");
+    cy.get("#checkBox").click();
+    cy.get("#button").should("be.disabled");
+    cy.get("#checkBox").click();
+    cy.get("#button").should("be.disabled");
+    cy.get("#textBox").type("Test");
+    cy.get("#checkBox").click();
+    cy.get("#button").should("not.be.disabled");
+    cy.get("#button").click();
+    cy.url().should("eq", "http://localhost:3000/dashboard"); // =
+  });
+});
