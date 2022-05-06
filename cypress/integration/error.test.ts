001bf193358ef6b096bc4deaadfab8e57519145b
Allan Hermann <allan.hermann@gmail.com>
2022-05-06 16:29:42 -0400

    chore: updated cypress tests to typescript

--- cypress/integration/error.test.ts
+++ cypress/integration/error.test.ts
@@ -2,6 +2,7 @@
 
 describe("Error Page", () => {
   it("should test the error page", () => {
+    //This test makes sure the erroe page is redirecting correctly
     cy.visit("http://localhost:3000/error");
     cy.get("a").click();
     cy.url().should("eq", "http://localhost:3000/login");
--- cypress/integration/login.test.ts
+++ cypress/integration/login.test.ts
@@ -2,6 +2,7 @@
 
 describe("Login Page", () => {
   it("should test the login page", () => {
+    //This test makes sure the button is only enabled when the form are filled
     cy.visit("http://localhost:3000");
     cy.get("#checkBox").click();
     cy.get("#button").should("be.disabled");
